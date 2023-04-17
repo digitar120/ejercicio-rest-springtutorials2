ba7ff3415c82ab58f3a7cff5d4d28bf7926b3bf3
digitar120 <pingas>
2023-04-17 15:52:32 -0300

    Creado un nuevo paquete 'repository'. Movida la interfaz 'InterfaceRepositorio' dentro de éste.

--- src/main/java/com/digitar120/registro/repository/InterfaceRepositorio.java
+++ src/main/java/com/digitar120/registro/repository/InterfaceRepositorio.java
@@ -1,5 +1,6 @@
-package com.digitar120.registro;
+package com.digitar120.registro.repository;
 
+import com.digitar120.registro.Registro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterfaceRepositorio extends JpaRepository<Registro, Integer>{
