aa37cfa74cbce109f7977ffedcc6681a204cd8eb
digitar120 <pingas>
2023-04-17 22:45:49 -0300

    Modificado el archivo 'schema.sql'.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-    INSERT INTO REGISTER VALUES (DEFAULT, 'Registro predeterminado 1');    
-    INSERT INTO REGISTER VALUES (DEFAULT, 'Registro predeterminado 2');    
+    INSERT INTO REGISTER VALUES (1, 'Registro predeterminado 1');    
+    INSERT INTO REGISTER VALUES (2, 'Registro predeterminado 2');    
